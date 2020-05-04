3b50e2e4af07806bd4026a051df9ae1e2890bba2
PUNAM-CODE <61118911+PUNAM-CODE@users.noreply.github.com>
2020-05-04 05:15:38 -0600

    Add files via upload

---
+++ tweet review.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  8 01:28:37 2020
+
+@author: admin
+"""
+
+
+import os
+import pandas as pd
+import tweepy
+import re
+import string
+from textblob import TextBlob
+#import preprocessor as p
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import csv
+import time
+from tweepy.auth import OAuthHandler
+
+
+
+#Twitter API credentials
+consumer_key = "LF7VkZ8QOrWF90piYGZypmjCh"
+consumer_secret = "v3e3hpLgc9Q6jws2vONFvUSdjFrzxVlhkDDHcMp5YxRRtF93ZG"
+access_key = "1247587088465145856-Hy0iRpsEcTNGXCxCd0snKI0KfZL1dI"
+access_secret = "g47R8LAo0u4xvBNbSjuPdsC9DCHvFQV47pHn0OGzParVO"
+
+alltweets = []
+
+def get_all_tweets(screen_name):
+    auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+    auth.set_access_token(access_key, access_secret)
+    api = tweepy.API(auth)
+    api.get_user('punam56118534').followers()
+    new_tweets = api.user_timeline(screen_name = screen_name,count=200)
+    alltweets.extend(new_tweets)
+   
+    oldest = alltweets[-1].id - 1
+    while len(new_tweets)>0:
+        new_tweets = api.user_timeline(screen_name = screen_name,count=200,max_id=oldest)
+        #save most recent tweets
+        alltweets.extend(new_tweets)
+        #update the id of the oldest tweet less one
+        oldest = alltweets[-1].id - 1
+        print ("...%s tweets downloaded so far" % (len(alltweets)))                # tweet.get('user', {}).get('location', {})
+ 
+    outtweets = [[tweet.created_at,tweet.entities["hashtags"],tweet.entities["user_mentions"],tweet.favorite_count,
+                  tweet.geo,tweet.id_str,tweet.lang,tweet.place,tweet.retweet_count,tweet.retweeted,tweet.source,tweet.text,
+                  tweet._json["user"]["location"],tweet._json["user"]["name"],tweet._json["user"]["time_zone"],
+                  tweet._json["user"]["utc_offset"]] for tweet in alltweets]
+   
+    import pandas as pd
+    tweets_df = pd.DataFrame(columns = ["time","hashtags","user_mentions","favorite_count",
+                                    "geo","id_str","lang","place","retweet_count","retweeted","source",
+                                    "text","location","name","time_zone","utc_offset"])
+    tweets_df["time"]  = pd.Series([str(i[0]) for i in outtweets])
+    tweets_df["hashtags"] = pd.Series([str(i[1]) for i in outtweets])
+    tweets_df["user_mentions"] = pd.Series([str(i[2]) for i in outtweets])
+    tweets_df["favorite_count"] = pd.Series([str(i[3]) for i in outtweets])
+    tweets_df["geo"] = pd.Series([str(i[4]) for i in outtweets])
+    tweets_df["id_str"] = pd.Series([str(i[5]) for i in outtweets])
+    tweets_df["lang"] = pd.Series([str(i[6]) for i in outtweets])
+    tweets_df["place"] = pd.Series([str(i[7]) for i in outtweets])
+    tweets_df["retweet_count"] = pd.Series([str(i[8]) for i in outtweets])
+    tweets_df["retweeted"] = pd.Series([str(i[9]) for i in outtweets])
+    tweets_df["source"] = pd.Series([str(i[10]) for i in outtweets])
+    tweets_df["text"] = pd.Series([str(i[11]) for i in outtweets])
+    tweets_df["location"] = pd.Series([str(i[12]) for i in outtweets])
+    tweets_df["name"] = pd.Series([str(i[13]) for i in outtweets])
+    tweets_df["time_zone"] = pd.Series([str(i[14]) for i in outtweets])
+    tweets_df["utc_offset"] = pd.Series([str(i[15]) for i in outtweets])
+    tweets_df.to_csv(screen_name+"_tweets.csv")
+    return tweets_df
+
+I_srk = get_all_tweets("Dronal trump")
+
